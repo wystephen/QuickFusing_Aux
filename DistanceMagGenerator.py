85b017f85fcb8a6e04527dca18728999ab520539
steve <551619855@qq.com>
2017-11-22 20:39:28 +0800

    norm mag distance.

--- DistanceMagGenerator.py
+++ DistanceMagGenerator.py
@@ -23,7 +23,6 @@
          佛祖保佑       永无BUG 
 '''
 
-
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
@@ -31,9 +30,8 @@ from mpl_toolkits.mplot3d import Axes3D
 
 from MagPreprocess import MagPreprocess
 
-
 if __name__ == '__main__':
-    v_data = np.loadtxt('/home/steve/Data/II/32/vertex_all_data.csv', delimiter=',')
+    v_data = np.loadtxt('/home/steve/Data/II/30/vertex_all_data.csv', delimiter=',')
 
     '''
             id | time ax ay az wx wy wz mx my mz pressure| x  y  z  vx vy vz| qx qy qz qw
@@ -41,54 +39,47 @@ if __name__ == '__main__':
             1 + 11 + 6 + 4 = 22
         '''
 
-
-
     fig = plt.figure()
-    ax = fig.add_subplot(111,projection='3d')
+    ax = fig.add_subplot(111, projection='3d')
 
-    ax.plot(v_data[:,12],v_data[:,13],v_data[:,14],'r-*')
+    ax.plot(v_data[:, 12], v_data[:, 13], v_data[:, 14], 'r-*')
 
+    mDetector = MagPreprocess.MagDetector(v_data[:, 8:11],
+                                          v_data[:, 12:15],
+                                          v_data[:, 11])
 
-    mDetector = MagPreprocess.MagDetector(v_data[:,8:11],
-                            v_data[:,12:15],
-                            v_data[:,11])
+    mDetector.Step2Length(35.0)
 
-    mDetector.Step2Length(30.0)
+    the_threshold = 2500
 
-    the_threshold = 1000
-
-    for i in range(mDetector.tmp_fft_mat.shape[0]):
+    for i in range(0, mDetector.tmp_fft_mat.shape[0]):
         for j in range(i, mDetector.tmp_fft_mat.shape[0]):
-            if mDetector.tmp_fft_mat[i,j] < the_threshold:
+            if mDetector.tmp_fft_mat[i, j] < the_threshold and \
+                            abs(mDetector.length_array[i] - mDetector.length_array[j]) > 30.0 and \
+                            mDetector.tmp_fft_mat[i, j] < 4999000 and \
+                            abs(v_data[i, 11] - v_data[j, 11]) < 1e10:
                 ax.plot(
-                    [v_data[i,12],v_data[j,12]],
-                    [v_data[i,13],v_data[j,13]],
-                    [v_data[i,14],v_data[j,14]],
-                    'b-',
-                    linewidth=0.1 #p.log2(mDetector.tmp_fft_mat[i,j])[0,0]
-                )
-
-
-    plt.figure()
-    plt.grid()
-    plt.title('trace')
-    plt.plot(v_data[:,12],v_data[:,13],'r-*')
-
-    for i in range(mDetector.tmp_fft_mat.shape[0]):
-        for j in range(i, mDetector.tmp_fft_mat.shape[0]):
-            if mDetector.tmp_fft_mat[i,j] < the_threshold:
-                plt.plot(
-                    [v_data[i,12],v_data[j,12]],
-                    [v_data[i,13],v_data[j,13]],
+                    [v_data[i, 12], v_data[j, 12]],
+                    [v_data[i, 13], v_data[j, 13]],
+                    [v_data[i, 14], v_data[j, 14]],
                     'b--',
-                    linewidth=0.1
+                    linewidth=0.1  # p.log2(mDetector.tmp_fft_mat[i,j])[0,0]
                 )
 
-
-
-
-
-
-
+    # plt.figure()
+    # plt.grid()
+    # plt.title('trace')
+    # plt.plot(v_data[:, 12], v_data[:, 13], 'r-*')
+    #
+    # for i in range(mDetector.tmp_fft_mat.shape[0]):
+    #     for j in range(i, mDetector.tmp_fft_mat.shape[0]):
+    #         if mDetector.tmp_fft_mat[i, j] < the_threshold and \
+    #                         abs(mDetector.length_array[i] - mDetector.length_array[j]) > 30.0:
+    #             plt.plot(
+    #                 [v_data[i, 12], v_data[j, 12]],
+    #                 [v_data[i, 13], v_data[j, 13]],
+    #                 'b--',
+    #                 linewidth=0.1
+    #             )
 
     plt.show()
--- MagPreprocess/MagPreprocess.py
+++ MagPreprocess/MagPreprocess.py
@@ -91,16 +91,23 @@ class MagDetector:
         plt.title('dis fft')
 
         self.tmp_fft_mat = np.zeros([self.mag_fft_feature.shape[0],
-                                self.mag_fft_feature.shape[0]])+10000
+                                     self.mag_fft_feature.shape[0]]) + 10000000000
 
         for i in range(self.mag_fft_feature.shape[0]):
             for j in range(i, self.mag_fft_feature.shape[0]):
+
+                if np.linalg.norm(self.mag_fft_feature[i, :] - np.zeros([
+                    1, len(test_shape_fft)], dtype=np.complex
+                )) < 10.0:
+                    continue
+
                 self.tmp_fft_mat[i, j] = np.linalg.norm(
                     self.mag_fft_feature[i, :] - self.mag_fft_feature[j, :]
                 )
 
-                if (self.tmp_fft_mat[i, j] > 4000):
-                    self.tmp_fft_mat[i, j] = 5000
+                # if (self.tmp_fft_mat[i, j] > 4000):
+                #     self.tmp_fft_mat[i, j] = 5000
+                self.tmp_fft_mat[j, i] = self.tmp_fft_mat[i, j]
 
         plt.imshow(self.tmp_fft_mat)
         plt.colorbar()

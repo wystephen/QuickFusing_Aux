266fcf741376c2ac0fa3dfc43a209a9d0977e612
steve <551619855@qq.com>
2017-11-30 21:01:56 +0800

    speed up by adopted a scipy inline function for compute distance.

--- DistanceMagGenerator.py
+++ DistanceMagGenerator.py
@@ -36,7 +36,7 @@ import time
 if __name__ == '__main__':
 
     start_time = time.time()
-    dir_name = '/home/steve/Data/II/20/'
+    dir_name = '/home/steve/Data/II/17/'
 
     ### key 16 17 20 ||| 28  30  (31)
     ##  33 34 35

2c75cdc22ea77e1f39eb7eae70eda13cdedadb8f
steve <551619855@qq.com>
2017-11-30 19:07:55 +0800

    speed up by adopted a scipy inline function for compute distance.

--- ShowData/TestAttitude.py
+++ ShowData/TestAttitude.py
@@ -23,7 +23,6 @@
          佛祖保佑       永无BUG 
 '''
 
-
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
@@ -66,84 +65,78 @@ if __name__ == '__main__':
     #
     # ax.plot(v_data[:, 12], v_data[:, 13], v_data[:, 14], 'r-*')
 
-    qeuler = np.zeros([v_data.shape[0],3])
+    qeuler = np.zeros([v_data.shape[0], 3])
     for i in range(v_data.shape[0]):
-        qeuler[i,:] = quat2euler([v_data[i,21],v_data[i,18],v_data[i,19],v_data[i,20]])
+        qeuler[i, :] = quat2euler([v_data[i, 21], v_data[i, 18], v_data[i, 19], v_data[i, 20]])
 
-    pangle = np.zeros([v_data.shape[0],3])
-    for i in range(v_data.shape[0]-1):
-        t = v_data[i+1,12:15]-v_data[i,12:15]
-        pangle[i,2] = np.arctan2(t[1],t[0])
-
+    pangle = np.zeros([v_data.shape[0], 3])
+    for i in range(v_data.shape[0] - 1):
+        t = v_data[i + 1, 12:15] - v_data[i, 12:15]
+        pangle[i, 2] = np.arctan2(t[1], t[0])
 
     mangle = mDetector.direct_mag_angle
-    relative_mangle = mangle[:,0]+pangle[:,2]-qeuler[:,2]
+    relative_mangle = mangle[:, 0] + pangle[:, 2] - qeuler[:, 2]
 
-
-    angle_dis = squareform(pdist(mangle,lambda u,v:np.arcsin(np.abs(np.sin(u-v)))/np.pi*180.0))
+    angle_dis = squareform(pdist(mangle, lambda u, v: np.arcsin(np.abs(np.sin(u - v))) / np.pi * 180.0))
 
     angle_dis_dis = squareform(pdist(angle_dis))
+    # np.vectorize(lambda x:x if x<100 else x=100)(angle_dis_dis)
+    np.where(angle_dis_dis < 100, angle_dis_dis, 100)
 
     # relative_mangle = np.arcsin(np.abs(np.sin(relative_mangle)))/np.pi * 180.0
-    relative_mangle /=(np.pi/180.0)
-    mangle /=(np.pi/180.0)
-
-
+    relative_mangle /= (np.pi / 180.0)
+    mangle /= (np.pi / 180.0)
 
     qeuler /= (np.pi / 180.0)
-    pangle /= (np.pi /180.0)
-
+    pangle /= (np.pi / 180.0)
 
     plot_row = 2
     plot_col = 3
     plt.figure()
-    plt.subplot(plot_row,plot_col,1)
+    plt.subplot(plot_row, plot_col, 1)
     # plt.title('qeuler')
     for i in range(qeuler.shape[1]):
-        plt.plot(qeuler[:,i],'-+',label='qangle '+str(i))
-    plt.legend();plt.grid()
-
+        plt.plot(qeuler[:, i], '-+', label='qangle ' + str(i))
+    plt.legend();
+    plt.grid()
 
     # plt.figure()
     # plt.subplot(plot_row,plot_col,2)
     # plt.title('pangle')
     for i in range(pangle.shape[1]):
-        plt.plot(pangle[:,i],'-+',label='pangle '+str(i))
-    plt.legend();plt.grid()
-
+        plt.plot(pangle[:, i], '-+', label='pangle ' + str(i))
+    plt.legend();
+    plt.grid()
 
     # plt.figure()
     # plt.subplot(plot_row,plot_col,3)
     # plt.title('diff')
     for i in range(pangle.shape[1]):
-        plt.plot(qeuler[:,i]-pangle[:,i],'-+',label='diff '+str(i))
-    plt.legend();plt.grid()
-
-
-
-    # plt.figure()
-    plt.subplot(plot_row,plot_col,4)
-    plt.title('trace 2d')
-    plt.plot(v_data[:,12],v_data[:,13],'r--+')
+        plt.plot(qeuler[:, i] - pangle[:, i], '-+', label='diff ' + str(i))
+    plt.legend();
     plt.grid()
 
-    plt.subplot(plot_row,plot_col,5)
+    # plt.figure()
+    plt.subplot(plot_row, plot_col, 4)
+    plt.title('trace 2d')
+    plt.plot(v_data[:, 12], v_data[:, 13], 'r--+')
+    plt.grid()
+
+    plt.subplot(plot_row, plot_col, 5)
     plt.title('angle ')
-    plt.plot(mangle[:,0],label='mag angle')
-    plt.plot(relative_mangle,label='relative mag angle')
+    plt.plot(mangle[:, 0], label='mag angle')
+    plt.plot(relative_mangle, label='relative mag angle')
     plt.grid()
     plt.legend()
 
-    plt.subplot(plot_row,plot_col,6)
+    plt.subplot(plot_row, plot_col, 6)
     plt.title('angle distance')
     plt.imshow(angle_dis)
     plt.colorbar()
 
-    plt.subplot(plot_row,plot_col,2)
+    plt.subplot(plot_row, plot_col, 2)
     plt.title('dis of dis')
     plt.imshow(angle_dis_dis)
     plt.colorbar()
-
-
 
     plt.show()

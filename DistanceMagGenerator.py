a4d945264af4098ad2255a52eff8d84a83e6261a
steve <551619855@qq.com>
2017-11-30 08:34:17 +0800

    s

--- DistanceMagGenerator.py
+++ DistanceMagGenerator.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     # mDetector.GetZFFtDis(20.0)
     # mDetector.MultiLayerNZFFt([30, 25, 20.0, 15.0, 10.0, 5.0])
     # mDetector.GetRelativeAttDis(50.0)
-    mDetector.MultiLayerANZFFt([30.0,25.0,20.0,15.0,10.0,5.0])
+    mDetector.MultiLayerANZFFt([30.0, 25.0, 20.0, 15.0, 10.0, 5.0])
 
     the_threshold = 30
     max_dis = 30.0
@@ -97,8 +97,8 @@ if __name__ == '__main__':
             #                 abs(mDetector.length_array[i] - mDetector.length_array[j]) > max_dis and \
             #                 abs(v_data[i, 11] - v_data[j, 11]) < 1e11:
             if mDetector.tmp_mnza_mat[i, j] < 10.0 and \
-                abs(mDetector.length_array[i] - mDetector.length_array[j]) > max_dis and \
-                abs(v_data[i, 11] - v_data[j, 11]) < 1e11:
+                    abs(mDetector.length_array[i] - mDetector.length_array[j]) > max_dis and \
+                    abs(v_data[i, 11] - v_data[j, 11]) < 1e11:
                 ax.plot(
                     [v_data[i, 12], v_data[j, 12]],
                     [v_data[i, 13], v_data[j, 13]],
@@ -110,7 +110,7 @@ if __name__ == '__main__':
     # plt.title('mag att feature')
     # plt.imshow(mDetector.mag_att_feature.transpose())
     # plt.colorbar()
-    print('totally time :', time.time()-start_time)
+    print('totally time :', time.time() - start_time)
 
     plt.figure()
     plt.title('hist of dis')

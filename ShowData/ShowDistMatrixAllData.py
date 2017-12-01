2a201b2b6339a6470462745bbc308d85d8868f3d
steve <551619855@qq.com>
2017-12-01 14:40:20 +0800

    speed up by adopted a scipy inline function for compute distance.

--- ShowData/ShowDistMatrixAllData.py
+++ ShowData/ShowDistMatrixAllData.py
@@ -22,3 +22,61 @@
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
          佛祖保佑       永无BUG 
 '''
+
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+from MagPreprocess import MagPreprocess
+
+import timeit
+import time
+
+if __name__ == '__main__':
+
+    dir_list = [16,17,20,28,34,35]
+    plt.figure()
+    plot_rows = 3
+    plot_cols = len(dir_list)
+    # plt.subplot()
+    for dir_i in range(len(dir_list)):
+        dir_str = dir_list[dir_i]
+        dir_name = 'home/steve/Data/II/'+str(dir_str)+"/"
+
+        v_data = np.loadtxt(dir_name + 'vertex_all_data.csv', delimiter=',')
+
+        '''
+                id | time ax ay az wx wy wz mx my mz pressure| x  y  z  vx vy vz| qx qy qz qw
+                0  |   1   2  3 4  5   6  7 8  9  10 11      | 12 13 14 15 16 17| 18 19 20 21
+                1 + 11 + 6 + 4 = 22
+            '''
+        mDetector = MagPreprocess.MagDetector(v_data[:, 8:11],
+                                              v_data[:, 2:5],
+                                              v_data[:, 12:],
+                                              v_data[:, 11])
+
+        mDetector.Step2Length(False)
+        # mDetector.GetFFTDis(20.0)
+        # mDetector.MultiLayerNormFFt([30.0, 25.0, 20.0, 15.0, 10.0, 5.0])
+        # mDetector.GetDirectDis(20.0)
+        mDetector.GetZValue(False)
+        mDetector.ConvertMagAttitude()
+        # mDetector.GetZFFtDis(20.0)
+        # mDetector.MultiLayerNZFFt([30, 25, 20.0, 15.0, 10.0, 5.0])
+        # mDetector.GetRelativeAttDis(50.0)
+        mDetector.MultiLayerANZFFt([30.0, 25.0, 20.0, 15.0, 10.0, 5.0])
+
+        plt.subplot(plot_rows,plot_cols,dir_i+1)
+        plt.title(dir_str+' trace')
+        plt.plot(v_data[:,12],v_data[:,13],'r--+')
+        plt.grid()
+        plt.subplot(plot_rows,plot_cols,dir_i+1+plot_cols)
+        plt.title(dir_str+' dis_matrix')
+        plt.imshow(mDetector.tmp_mnza_mat)
+        plt.colorbar()
+
+
+
+    plt.show()

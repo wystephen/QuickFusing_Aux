357d59a7b5d48aa1265d22ef04b1c3f3a420ac5a
steve <551619855@qq.com>
2017-12-05 09:44:09 +0800

    speed up by adopted a scipy inline function for compute distance.

--- ShowData/TryinverseSort.py
+++ ShowData/TryinverseSort.py
@@ -82,7 +82,7 @@ if __name__ == '__main__':
     cv2.namedWindow('the')
     # cv2.namedWindow('the2')
     cv2.createTrackbar('threshold', 'the', 0, 500, lambda x: x)
-    cv2.createTrackbar('line_len','the',0,2550,lambda y:y)
+    cv2.createTrackbar('line_len','the',2220,2550,lambda y:y)
     cv2.createTrackbar('line_gap','the',0,2550,lambda x:x)
     cv2.createTrackbar('c_size','the',1,50,lambda x:x)
     cv2.createTrackbar('ero_size','the',1,50,lambda x:x)

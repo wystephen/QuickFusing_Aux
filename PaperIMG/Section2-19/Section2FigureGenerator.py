1d0b9e008520d58142f19d9dd8f149e2cb9d26d3
steve <551619855@qq.com>
2017-12-14 10:40:57 +0800

    First version of pairs method.

--- PaperIMG/Section2-19/Section2FigureGenerator.py
+++ PaperIMG/Section2-19/Section2FigureGenerator.py
@@ -30,6 +30,9 @@ import scipy as sp
 import numpy as np
 import seaborn
 
+
+from scipy.spatial.distance import pdist, squareform
+
 if __name__ == '__main__':
     bi_mat = np.loadtxt('19bi_mat.data')
     mnza_mat = np.loadtxt('19mnza_mat.data')
@@ -41,25 +44,36 @@ if __name__ == '__main__':
     trace_imu = np.loadtxt('text_imu.txt', delimiter=',')
     trace_graph = np.loadtxt('test.txt', delimiter=',')
 
+
+    ref_dis_mat = np.zeros([trace_graph.shape[0],trace_graph.shape[0]])
+
+    ref_dis_mat = squareform(pdist(trace_graph))
+
     plt.subplot(1, 3, 2)
     plt.title('bi_mat')
     plt.imshow(bi_mat)
-    plt.colorbar()
+    # plt.colorbar()
 
     plt.subplot(1, 3, 1)
     plt.title('feature_mat')
     plt.imshow(mnza_mat)
-    plt.colorbar()
+    # plt.colorbar()
 
     plt.subplot(1, 3, 3)
     plt.title('pairs_mat')
     plt.imshow(pairs_mat)
-    plt.colorbar()
+    # plt.colorbar()
 
     plt.figure()
     plt.plot(trace_imu[:, 0], trace_imu[:, 1], '+-', label='trace_imu')
     plt.plot(trace_graph[:, 0], trace_graph[:, 1], '+-', label='trace graph')
     plt.grid()
     plt.legend()
+
+    plt.figure()
+    ref_dis_mat = np.where(ref_dis_mat<1.0,ref_dis_mat,2.0)
+    plt.imshow(ref_dis_mat)
+    plt.title('ref distance mat')
+    plt.colorbar()
 
     plt.show()

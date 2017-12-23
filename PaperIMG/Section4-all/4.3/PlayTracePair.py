b37bfb088c1aa1544662dfe2dbf9a386b011953e
steve <551619855@qq.com>
2017-12-23 21:17:54 +0800

    Begin coding for Section4.1. ~sure~

--- PaperIMG/Section4-all/4.3/PlayTracePair.py
+++ PaperIMG/Section4-all/4.3/PlayTracePair.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-# Created by steve @ 17-12-23 上午10:32
+# Created by steve @ 17-12-23 下午9:06
 '''
                    _ooOoo_ 
                   o8888888o 
@@ -23,20 +23,28 @@
          佛祖保佑       永无BUG 
 '''
 
-import scipy as sp
-import numpy as np
 
 import matplotlib.pyplot as plt
+import scipy as sp
+import  numpy as np
+
+import os
+import  time
 
 
-class TraceObject:
-    def __init__(self, trace_3d):
-        '''
-        Find out shape of trace(a rectangle).
+if __name__ == '__main__':
+    dir_num_list = [19,28,33,34]
+    for dir_num in dir_num_list:
+        graph_trace = np.loadtxt('../'+str(dir_num)+'/test.txt',delimiter=',')
+        imu_trace = np.loadtxt('../'+str(dir_num)+'/text_imu.txt',delimiter=',')
+        pair = np.loadtxt('../'+str(dir_num)+'/pair.txt',delimiter=',')
 
-        :param trace_3d:
-        '''
-        self.trace_3d = trace_3d
-
+        plt.figure()
+        plt.plot(graph_trace[:,0],graph_trace[:,1],label='graph-optimized')
+        plt.plot(imu_trace[:,0],imu_trace[:,1],label='zupt')
+        plt.grid()
+        plt.legend()
+        plt.savefig(str(dir_num)+'trace.jpg',dpi=1000)
+    plt.show()
 
 

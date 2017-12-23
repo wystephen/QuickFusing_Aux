3d4d51a75a6f090764ef715c93a9e369498e49ff
steve <551619855@qq.com>
2017-12-23 19:48:05 +0800

    Begin coding for Section4.1. ~sure~

---
+++ PaperIMG/Section4-all/4.1SourceVSFFT/AllTrajectory.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+# Created by steve @ 17-12-23 下午7:46
+'''
+                   _ooOoo_ 
+                  o8888888o 
+                  88" . "88 
+                  (| -_- |) 
+                  O\  =  /O 
+               ____/`---'\____ 
+             .'  \\|     |//  `. 
+            /  \\|||  :  |||//  \ 
+           /  _||||| -:- |||||-  \ 
+           |   | \\\  -  /// |   | 
+           | \_|  ''\---/''  |   | 
+           \  .-\__  `-`  ___/-. / 
+         ___`. .'  /--.--\  `. . __ 
+      ."" '<  `.___\_<|>_/___.'  >'"". 
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | | 
+     \  \ `-.   \_ __\ /__ _/   .-` /  / 
+======`-.____`-.___\_____/___.-`____.-'====== 
+                   `=---=' 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+         佛祖保佑       永无BUG 
+'''
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import pdist, squareform
+
+from MagPreprocess import MagPreprocess
+
+import os
+
+import Auc

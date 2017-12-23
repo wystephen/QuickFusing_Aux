b425b81cd6c8e3021dd0449725ca356ed8f0a8f3
steve <551619855@qq.com>
2017-12-23 20:51:52 +0800

    Begin coding for Section4.1. ~sure~

--- PaperIMG/Section4-all/4.1SourceVSFFT/Section4.2Generator.py
+++ PaperIMG/Section4-all/4.1SourceVSFFT/Section4.2Generator.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-# Created by steve @ 17-12-23 下午7:46
+# Created by steve @ 17-12-23 下午8:44
 '''
                    _ooOoo_ 
                   o8888888o 
@@ -22,7 +22,6 @@
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
          佛祖保佑       永无BUG 
 '''
-
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
@@ -39,38 +38,23 @@ if __name__ == '__main__':
     auc_list = list()
     start_auc_time = time.time()
     print()
-    for data_num in [ 20, 28, 33, 34]:
+    for data_num in [20, 28, 33, 34]:
         t_auc = AucBuilder.AUCBuilder(str(data_num))
-        t_auc.compute_ref_mat(is_show=False)
-        t_auc.compute_all_auc(is_show=False)
+        t_auc.compute_ref_mat(distance_threshold=2.0, is_show=False)
         auc_list.append(t_auc)
-    print(time.time()-start_auc_time)
+    print(time.time() - start_auc_time)
 
-    # begin plot
-    for auc in auc_list:
-        plt.figure(figsize=(7.5,7.5))
-        plt.subplot(2,2,1)
+    for index, auc in enumerate(auc_list):
+        plt.figure(figsize=(8.0, 4.0))
+        plt.subplot(1, 3, 1)
         plt.title('(a)')
-        plt.plot(auc.trace_path[:,0],auc.trace_path[:,1],'-*')
-        plt.grid()
-        plt.subplot(2,2,3)
-        plt.title('(c)')
-        plt.imshow(auc.src_mat[15:-15,15:-15])
-        # plt.grid()
-        plt.subplot(2,2,4)
-        plt.title('(d)')
-        plt.imshow(auc.mnza_mat[15:-15,15:-15])
-        # plt.grid()
-        plt.subplot(2,2,2)
+        plt.imshow(auc.ref_mat[15:-15, 15:-15])
+        plt.subplot(1, 3, 2)
         plt.title('(b)')
-        plt.plot(auc.mnza_FPR, auc.mnza_TPR, '*', label='MultiLayerFFT')
-        plt.plot(auc.src_FPR, auc.src_TPR, '*', label='Original')
-        plt.legend()
-        plt.grid()
-        plt.axis([0,1,0,1])
+        plt.imshow(auc.bi_mat[15:-15, 15:-15])
+        plt.subplot(1, 3, 2)
+        plt.title('(c)')
+        plt.imshow(auc.result_mat[15:-15, 15:-15])
 
-        plt.savefig(auc.dir_name[:-1]+'data.jpg', dpi = 1000)
+        plt.savefig(auc.dir_name[:-1] + '4-2.jpg', dpi=1000)
     plt.show()
-
-
-

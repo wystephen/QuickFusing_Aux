2d5032190edf1b562e46b8561b52b4fdff401a7b
steve <551619855@qq.com>
2017-12-24 14:16:05 +0800

    Begin coding for Section4.1. ~sure~

--- PaperIMG/Section4-all/4.CompureDiffRobust/DifferenceOfRobustKernel.py
+++ PaperIMG/Section4-all/4.CompureDiffRobust/DifferenceOfRobustKernel.py
@@ -23,39 +23,36 @@
          佛祖保佑       永无BUG 
 '''
 
-import  numpy as np
+import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 
-
 if __name__ == '__main__':
-    pair_mat = np.loadtxt('./DCS/pair.txt',delimiter=',')
+    pair_mat = np.loadtxt('./DCS/corner_pair.txt', delimiter=',')
     pair_mat = pair_mat.astype(dtype=np.int)
 
-    dcs_trace = np.loadtxt('./DCS/test.txt',delimiter=',')
+    dcs_trace = np.loadtxt('./DCS/test.txt', delimiter=',')
 
-    robust_trace = np.loadtxt('./Robust/test.txt',delimiter=',')
+    robust_trace = np.loadtxt('./Robust/test.txt', delimiter=',')
 
     plt.figure()
-    plt.subplot(1,2,1)
+    plt.subplot(1, 2, 1)
     plt.grid()
-    plt.plot(dcs_trace[:,0],dcs_trace[:,1],label='path')
+    plt.plot(dcs_trace[:, 0], dcs_trace[:, 1], label='path')
     for i in range(pair_mat.shape[0]):
         v = pair_mat[i,:]
-        plt.plot([dcs_trace[v[0],0],dcs_trace[v[1],0]],
-                 [dcs_trace[v[0],0],dcs_trace[v[1],1]],
+        plt.plot(np.asarray([dcs_trace[v[0],0],dcs_trace[v[1],0]]),
+                 np.asarray([dcs_trace[v[0],1],dcs_trace[v[1],1]]),
                  '--g')
     plt.legend()
 
-    plt.subplot(1,2,2)
+    plt.subplot(1, 2, 2)
     plt.grid()
-    plt.plot(robust_trace[:,0],robust_trace[:,1],label='paht')
-    # for i in range(pair_mat.shape[0]):
-    #     v=pair_mat[i,:].astype(dtype=np.int)
-    #     plt.plot([robust_trace[v[0],0],robust_trace[v[1],0]],
-    #              [robust_trace[v[0],1],robust_trace[v[1],1]],
-    #              '--g',label='pairs')
+    plt.plot(robust_trace[:, 0], robust_trace[:, 1], label='paht')
+    for i in range(pair_mat.shape[0]):
+        v = pair_mat[i, :]
+        plt.plot(np.asarray([robust_trace[v[0], 0], robust_trace[v[1], 0]]),
+                 np.asarray([robust_trace[v[0], 1], robust_trace[v[1], 1]]),
+                 '--g')
     plt.legend()
     plt.show()
-
-

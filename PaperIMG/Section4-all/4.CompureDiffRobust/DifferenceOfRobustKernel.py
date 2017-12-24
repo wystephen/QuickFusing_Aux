cf79cc0be76bbe8b8398c5c9e2d4d3d469f68803
steve <551619855@qq.com>
2017-12-24 14:35:00 +0800

    Begin coding for Section4.1. ~sure~

--- PaperIMG/Section4-all/4.CompureDiffRobust/DifferenceOfRobustKernel.py
+++ PaperIMG/Section4-all/4.CompureDiffRobust/DifferenceOfRobustKernel.py
@@ -26,6 +26,7 @@
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 if __name__ == '__main__':
     pair_mat = np.loadtxt('./DCS/corner_pair.txt', delimiter=',')
@@ -35,24 +36,50 @@ if __name__ == '__main__':
 
     robust_trace = np.loadtxt('./Robust/test.txt', delimiter=',')
 
-    plt.figure()
-    plt.subplot(1, 2, 1)
+    plt.figure(figsize=(8,4))
+    plt.subplot(1, 3, 1)
+    plt.title('(a)')
     plt.grid()
     plt.plot(dcs_trace[:, 0], dcs_trace[:, 1], label='path')
     for i in range(pair_mat.shape[0]):
-        v = pair_mat[i,:]
-        plt.plot(np.asarray([dcs_trace[v[0],0],dcs_trace[v[1],0]]),
-                 np.asarray([dcs_trace[v[0],1],dcs_trace[v[1],1]]),
+        v = pair_mat[i, :]
+        plt.plot(np.asarray([dcs_trace[v[0], 0], dcs_trace[v[1], 0]]),
+                 np.asarray([dcs_trace[v[0], 1], dcs_trace[v[1], 1]]),
                  '--g')
     plt.legend()
 
-    plt.subplot(1, 2, 2)
+    fig = plt.subplot(1, 3, 2)
+    fig.set_title('(b)')
+    fig.grid()
+    fig.plot(robust_trace[:, 0], robust_trace[:, 1], label='path')
+    for i in range(pair_mat.shape[0]):
+        v = pair_mat[i, :]
+        fig.plot(np.asarray([robust_trace[v[0], 0], robust_trace[v[1], 0]]),
+                 np.asarray([robust_trace[v[0], 1], robust_trace[v[1], 1]]),
+                 '--g')
+
+    # t_rectangle = plt.Rectangle([0.0, -6], width=15, height=8)
+    # plt.add_path(t_rectangle.getpath())
+    fig.add_patch(
+        patches.Rectangle(
+            (0.0,-6),
+            15,8, fill=False
+        )
+    )
+
+
+    plt.legend()
+    plt.subplot(1, 3, 3)
+    plt.title('(c)')
     plt.grid()
-    plt.plot(robust_trace[:, 0], robust_trace[:, 1], label='paht')
+    plt.plot(robust_trace[:, 0], robust_trace[:, 1], label='path')
     for i in range(pair_mat.shape[0]):
         v = pair_mat[i, :]
         plt.plot(np.asarray([robust_trace[v[0], 0], robust_trace[v[1], 0]]),
                  np.asarray([robust_trace[v[0], 1], robust_trace[v[1], 1]]),
                  '--g')
-    plt.legend()
+    plt.axis([0.0, 15, -6, 2.0])
+
+    plt.savefig('compare_fig.jpg',dpi=1000)
+
     plt.show()

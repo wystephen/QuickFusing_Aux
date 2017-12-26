b1d7f1b52d88d73fcfb0d9ef20e614836cd9c528
steve <551619855@qq.com>
2017-12-26 10:11:54 +0800

    Begin coding for Section4.1. ~sure~

---
+++ PaperIMG/DirectFFTCompare/Compare.py
@@ -0,0 +1,69 @@
+# -*- coding:utf-8 -*-
+# Created by steve @ 17-12-26 上午10:11
+'''
+                   _ooOoo_ 
+                  o8888888o 
+                  88" . "88 
+                  (| -_- |) 
+                  O\  =  /O 
+               ____/`---'\____ 
+             .'  \\|     |//  `. 
+            /  \\|||  :  |||//  \ 
+           /  _||||| -:- |||||-  \ 
+           |   | \\\  -  /// |   | 
+           | \_|  ''\---/''  |   | 
+           \  .-\__  `-`  ___/-. / 
+         ___`. .'  /--.--\  `. . __ 
+      ."" '<  `.___\_<|>_/___.'  >'"". 
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | | 
+     \  \ `-.   \_ __\ /__ _/   .-` /  / 
+======`-.____`-.___\_____/___.-`____.-'====== 
+                   `=---=' 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+         佛祖保佑       永无BUG 
+'''
+
+
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+from MagPreprocess import MagPreprocess
+
+import seaborn as sns
+
+
+import timeit
+import time
+
+if __name__ == '__main__':
+    sns.set('paper','white')
+
+    start_time = time.time()
+    dir_name = '/home/steve/Data/II/34/'
+
+    ### key 16 17 20 ||| 28  30  (31)
+    ##  33 34 35
+
+    v_data = np.loadtxt(dir_name + 'vertex_all_data.csv', delimiter=',')
+
+    '''
+            id | time ax ay az wx wy wz mx my mz pressure| x  y  z  vx vy vz| qx qy qz qw
+            0  |   1   2  3 4  5   6  7 8  9  10 11      | 12 13 14 15 16 17| 18 19 20 21
+            1 + 11 + 6 + 4 = 22
+        '''
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.plot(v_data[:, 12], v_data[:, 13], v_data[:, 14], '-*',label='trace 3d \\alpha ')
+    ax.legend()
+
+    mDetector = MagPreprocess.MagDetector(v_data[:, 8:11],
+                                          v_data[:, 2:5],
+                                          v_data[:, 12:],
+                                          v_data[:, 11])
+
+    mDetector.Step2Length(False)

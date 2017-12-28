243800300f953d2ce5e22e6a93cd4e1c99210d84
steve <551619855@qq.com>
2017-12-28 20:51:22 +0800

    Begin coding for Section4.1. ~sure~

--- ShowData/TraceRecDetector.py
+++ ShowData/TraceRecDetector.py
@@ -18,8 +18,8 @@
      | | :  `- \`.;`\ _ /`;.`/ - ` : | | 
      \  \ `-.   \_ __\ /__ _/   .-` /  / 
 ======`-.____`-.___\_____/___.-`____.-'====== 
-                   `=---=' 
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+                   `=---='
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
          佛祖保佑       永无BUG 
 '''
 
@@ -27,6 +27,10 @@ import scipy as sp
 import numpy as np
 
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import time
+import os
+import seaborn
 
 
 class TraceObject:
@@ -38,5 +42,28 @@ class TraceObject:
         '''
         self.trace_3d = trace_3d
 
+    def find_rotation(self):
+        '''
+        find
+        :return:
+        '''
 
 
+
+
+if __name__ == '__main__':
+    trace = np.loadtxt('../PaperIMG/Section4-all/28/test.txt',
+                       delimiter=',')
+
+    # plt.figure()
+    fig_trace = plt.figure()
+    ax = fig_trace.add_subplot(111,projection='3d')
+
+    plt.figure()
+    plt.plot(trace[:, 0], trace[:, 1])
+    plt.grid()
+
+
+    ax.plot(trace[:,0],trace[:,1],trace[:,2],'*-')
+    ax.grid()
+    plt.show()

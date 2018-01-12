787e739ed17406760bc721bd0b8b0d558e699508
steve <551619855@qq.com>
2018-01-12 10:43:07 +0800

    Begin coding for Section4.1. ~sure~

--- PaperIMG/Section4-all/4.3/AccuracyTableGenerator.py
+++ PaperIMG/Section4-all/4.3/AccuracyTableGenerator.py
@@ -22,3 +22,34 @@
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
          佛祖保佑       永无BUG 
 '''
+import matplotlib.pyplot as plt
+import scipy as sp
+import numpy as np
+
+import os
+import time
+
+from ShowData import TraceProcess
+from ShowData import TraceCompare
+
+if __name__ == '__main__':
+    dir_num_list = [19, 20, 28, 33, 34]
+    for dir_num in dir_num_list:
+        graph_trace = np.loadtxt('../' + str(dir_num) + '/test.txt', delimiter=',')
+        imu_trace = np.loadtxt('../' + str(dir_num) + '/text_imu.txt', delimiter=',')
+        pair = np.loadtxt('../' + str(dir_num) + '/pair.txt', delimiter=',')
+        ref_trace = np.loadtxt('../4.1SourceVSFFT/' + str(dir_num) + '/test.txt', delimiter=',')
+        if dir_num is 20:
+            ref_trace[:, 0] *= -1.0
+        plt.figure()
+        plt.plot(graph_trace[:, 0], graph_trace[:, 1], label='graph')
+        plt.plot(ref_trace[:, 0], ref_trace[:, 1], label='ref')
+        plt.legend()
+        plt.grid()
+        plt.title(str(dir_num))
+        print("size of ref:", ref_trace.shape[0], ' size of graph:', graph_trace.shape[0])
+        tc = TraceCompare.TraceCompare(graph_trace, ref_trace)
+
+
+
+    # plt.show()

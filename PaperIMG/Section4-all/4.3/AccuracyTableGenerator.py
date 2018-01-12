24021269d25e02067d9a882b926ffa00fc50237b
steve <551619855@qq.com>
2018-01-12 20:26:55 +0800

    Begin coding for Section4.1. ~sure~

--- PaperIMG/Section4-all/4.3/AccuracyTableGenerator.py
+++ PaperIMG/Section4-all/4.3/AccuracyTableGenerator.py
@@ -39,7 +39,8 @@ if __name__ == '__main__':
     for dir_num in dir_num_list:
         graph_trace = np.loadtxt('../' + str(dir_num) + '/test.txt', delimiter=',')
         imu_trace = np.loadtxt('../' + str(dir_num) + '/text_imu.txt', delimiter=',')
-        pair = np.loadtxt('../' + str(dir_num) + '/pair.txt', delimiter=',')
+        # pair = np.loadtxt('../' + str(dir_num) + '/pair.txt', delimiter=',')
+        pair = np.loadtxt('../4.1SourceVSFFT/'+str(dir_num)+'/'+str(dir_num)+'pairs_mat.data')
         ref_trace = np.loadtxt('../4.1SourceVSFFT/' + str(dir_num) + '/test.txt', delimiter=',')
         if dir_num is 20:
             ref_trace[:, 0] *= -1.0
@@ -58,8 +59,28 @@ if __name__ == '__main__':
         # plt.grid()
         # plt.title(str(dir_num))
         # print("size of ref:", ref_trace.shape[0], ' size of graph:', graph_trace.shape[0])
-        print(np.min(pair), np.max(pair))
+        # print(np.min(pair), np.max(pair))
+        min_pair = 1000000
+        max_pair = 0
+        t = np.zeros([graph_trace.shape[0], graph_trace.shape[0]])
+        for i in range(pair.shape[0]):
+            t[int(pair[i, 0]),int( pair[i, 1])] = 1.0
+            if ((pair[i, 1] - pair[i, 0]) > 15) and \
+                    15 < pair[i, 0] < graph_trace.shape[0] - 15 and \
+                    15 < pair[i, 1] < graph_trace.shape[0] - 15:
+                min_pair = min(min_pair, np.min(pair[i, :]))
+                max_pair = max(max_pair, np.max(pair[i, :]))
+        # plt.figure()
+        # plt.imshow(t)
+        min_pair = int(min_pair)
+        max_pair = int(max_pair)
+        print('min:', min_pair, 'max:', max_pair, 'size:', graph_trace.shape[0])
+
         tc = TraceCompare.TraceCompare(graph_trace, ref_trace)
         print('data:', dir_num, 'error:', tc.error)
+
+
+        tc2 = TraceCompare.TraceCompare(graph_trace[min_pair:max_pair,:],ref_trace[min_pair:max_pair,:])
+        print('data',dir_num,'paired error:',tc2.error)
 
     plt.show()
